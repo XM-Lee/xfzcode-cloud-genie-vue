5aa576d347036f7cd5491806d702566dbf73fa06
surun028 <64138370+surun028@users.noreply.github.com>
2023-08-14 17:55:09 +0800

    feat

---
+++ src/canvas-sign.s.ts
@@ -0,0 +1,6 @@
+// env.d.ts中
+
+// TODO: TS 无法主动发现模块，如果找不到模块，则需要在此使用 declare module 进行配置
+declare module 'element-plus/dist/locale/zh-cn.mjs'
+declare module 'element-plus/dist/locale/en.mjs'
+declare module 'element-plus'
